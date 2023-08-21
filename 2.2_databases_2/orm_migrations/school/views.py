02c3a5be2a900a544461726eec45d42abefa7efd
Dmitriy Smirnov <git@d-sm.ru>
2023-08-21 15:44:23 +0300

    2.2.1 completed

--- 2.2_databases_2/orm_migrations/school/views.py
+++ 2.2_databases_2/orm_migrations/school/views.py
@@ -1,15 +1,18 @@
-from django.views.generic import ListView
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 from .models import Student
 
 
-def students_list(request):
+def students_list(request: HttpRequest) -> HttpResponse:
     template = 'school/students_list.html'
-    context = {}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
+    
+    context = {
+        'object_list': Student.objects.prefetch_related('teachers').all().order_by(ordering)
+    }
 
     return render(request, template, context)
--- 2.2_databases_2/orm_migrations/website/settings.py
+++ 2.2_databases_2/orm_migrations/website/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import db_conf
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -71,11 +74,19 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'netology_orm_migrations',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '5432',
+    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_orm_migrations',
-        'HOST': '127.0.0.1',
+        'NAME': db_conf.name,
+        'HOST': db_conf.host,
         'PORT': '5432',
+        'USER': db_conf.user,
+        'PASSWORD': db_conf.pwd
     }
 }
 
@@ -131,3 +142,19 @@ try:
 except ImportError:
     pass
 
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        }
+    }
+}
